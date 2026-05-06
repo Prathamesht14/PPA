9f164807d1ca75fe774cdcb554157cac5e866858
Prathamesh Tiparkar <tiparkarprathamesh96@gmail.com>
2026-05-07 00:19:39 +0530

    Added string handling C programs

---
+++ StringLengthUserdefined.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+// call by address
+int strlenX(char *ptr)
+{
+    int i = 0;
+
+    while(*ptr!='\0')
+    {
+        i++;
+        ptr++;
+    }
+    
+    return i;
+}
+
+int main()
+{
+   char str [] = "Ganesh";
+    int ret = 0;
+
+    ret = strlenX(str);
+    printf("String Length: %d\n",ret);
+
+    return 0;
+}
